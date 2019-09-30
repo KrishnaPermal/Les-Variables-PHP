04dd0e257dcbca4e449d048824f4f152803f2bd2
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2019-09-30 11:41:10 +0400

    second commit

---
+++ exo2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exo2</title>
+</head>
+<body>
+    
+    <?php 
+    
+    //Déclaration des variables
+    $firstname = "Jean";  
+    $lastname = "Brice";    
+    $age = 19;
+
+    echo $firstname;
+    echo '<br/>';
+    echo $lastname;
+    echo '<br/>';
+    echo $age;
+
+    ?>
+
+
+</body>
+</html>
---
+++ exo3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exo3</title>
+</head>
+<body>
+    
+    <?php 
+    
+
+        //Déclaration des variables
+        $km = 1;  
+        echo $km;
+
+        echo '<br/>';
+
+        $km = 3;
+        echo $km;
+
+        echo '<br/>';
+
+        $km = 125;
+        echo $km;
+       
+    
+        ?>
+    
+    
+    </body>
+    </html>
+
+ 
+
+
