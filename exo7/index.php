14338b399454fd6740f943d55ad24ce77b1f0f40
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2019-09-30 14:32:57 +0400

    five commit

---
+++ exo7/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exo7</title>
+</head>
+<body>
+    
+    <?php
+
+    //Déclaration des variables
+    $lastname = "DUPONT";  
+    $firstname = "Charly";    
+    $age = 24;
+
+    echo "Bonjour " . $lastname. $firstname . " , tu as ". 19 ." ans " ?>
+
+
+</body>
+</html>
---
+++ exo8/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exo8</title>
+</head>
+<body>
+
+    <?php
+
+    $a = 3 + 4 ;
+    $b = 5 * 20 ;
+    $c = 45 / 5 ;
+
+
+    echo $a ;
+    echo "<br/>";
+    echo "<br/>";
+    echo $b ;
+    echo "<br/>";
+    echo "<br/>";
+    echo $c ;
+
+
+    ?>
+
+
+    
+</body>
+</html>
